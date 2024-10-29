e3abde6546e409b285befa1af8fce012eff492fa
ValckeMalo <malovalcke@gmail.com>
2024-10-29 15:32:51 +0100

    fct gen class

---
+++ Localization/Localization/GenrateClass.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Localization
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private void GenerateCSharpClass()
+        {
+
+        }
+
+        private void GenerateCPlusPlusClass()
+        {
+
+        }
+    }
+}
