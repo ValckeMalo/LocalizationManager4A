fa41f530da0f4abe213fa2c9f246a33d706c4f86
ValckeMalo <malovalcke@gmail.com>
2024-11-12 16:01:37 +0100

    modif

--- Localization/Localization/LocalizationItem.cs
+++ Localization/Localization/LocalizationItem.cs
@@ -1,71 +1,72 @@
 ﻿using System.ComponentModel;
+using System.Collections.Generic;
 
 public class LocalizationItem : INotifyPropertyChanged
 {
-	private string language = string.Empty;
-	private StringValueKey stringsCollections;
+    private string language = string.Empty;
+    private List<StringValueKey> stringsCollections;
 
-    public LocalizationItem(string language, StringValueKey stringsCollections)
-	{
-		this.language = language;
-		this.stringsCollections = stringsCollections;
-	}
+    public LocalizationItem(string language, List<StringValueKey> stringsCollections)
+    {
+        this.language = language;
+        this.stringsCollections = stringsCollections;
+    }
 
-	public string Language
-	{
-		get => language;
-		set
-		{
-			if (language != value)
-			{
+    public string Language
+    {
+        get => language;
+        set
+        {
+            if (language != value)
+            {
                 language = value;
-				OnPropertyChanged(nameof(language));
-			}
-		}
-	}
+                OnPropertyChanged(nameof(language));
+            }
+        }
+    }
 
-	public StringValueKey StringsCollections
-	{
-		get => stringsCollections;
-		set
-		{
-			if (stringsCollections != value)
-			{
-				stringsCollections = value;
-				OnPropertyChanged(nameof(stringsCollections));
-			}
-		}
-	}
+    public List<StringValueKey> StringsCollections
+    {
+        get => stringsCollections;
+        set
+        {
+            if (stringsCollections != value)
+            {
+                stringsCollections = value;
+                OnPropertyChanged(nameof(stringsCollections));
+            }
+        }
+    }
 
-	public event PropertyChangedEventHandler? PropertyChanged;
-	protected virtual void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected virtual void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 }
 
 public class StringValueKey : INotifyPropertyChanged
 {
-	private string key = string.Empty;
-	private string value = string.Empty;
+    private string key = string.Empty;
+    private string value = string.Empty;
 
-	public string Key
-	{
-		get => key;
-		set
-		{
-			this.key = value;
-			OnPropertyChanged(nameof(this.key));
-		}
-	}
+    public string Key
+    {
+        get => key;
+        set
+        {
+            this.key = value;
+            OnPropertyChanged(nameof(this.key));
+        }
+    }
 
-	public string Value
-	{
-		get => value;
-		set
-		{
-			this.value = value;
-			OnPropertyChanged(nameof(this.value));
-		}
-	}
+    public string Value
+    {
+        get => value;
+        set
+        {
+            this.value = value;
+            OnPropertyChanged(nameof(this.value));
+        }
+    }
 
-	public event PropertyChangedEventHandler? PropertyChanged;
-	protected virtual void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected virtual void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 }
