f7608aaa7037cc8015ced511ed107a2954669438
DorianDelbos <thesothief@gmail.com>
2024-11-19 16:02:07 +0100

    Dialogues boxs

--- Localization/Localization/MainWindow.xaml.cs
+++ Localization/Localization/MainWindow.xaml.cs
@@ -14,38 +14,5 @@
             InitializeComponent();
             InitializeDataGrid();
 		}
-
-		private void Debug_Click(object sender, RoutedEventArgs e)
-		{
-			UpdateTables(new List<LocalizationItem>()
-			{
-				new LocalizationItem("Fr", new List<StringValueKey>()
-				{
-					new StringValueKey()
-					{
-						Key = "Play",
-						Value = "Jouer"
-					},
-					new StringValueKey()
-					{
-						Key = "Settings",
-						Value = "Options"
-					}
-				}),
-				new LocalizationItem("En", new List<StringValueKey>()
-				{
-					new StringValueKey()
-					{
-						Key = "Play",
-						Value = "Play"
-					},
-					new StringValueKey()
-					{
-						Key = "Settings",
-						Value = "Settings"
-					}
-				})
-			});
-		}
 	}
 }
--- Localization/Localization/MenuItemInterface.cs
+++ Localization/Localization/MenuItemInterface.cs
@@ -1,4 +1,5 @@
 ﻿using Localization.Plugin;
+using Microsoft.Win32;
 using System.Windows;
 
 namespace Localization
@@ -11,58 +12,160 @@ namespace Localization
         }
 
         private void ImportCsvButton_Click(object sender, RoutedEventArgs e)
-        {
-			UpdateTables(ImportFromCSV("CSV/masterCSV.csv"));
+		{
+			OpenFileDialog dialog = new OpenFileDialog();
+			dialog.FileName = "Import CSV file";
+			dialog.DefaultExt = ".csv";
+			dialog.Filter = "CSV file (.csv)|*.csv";
+
+			if (dialog.ShowDialog() == true)
+				UpdateTables(ImportFromCSV(dialog.FileName));
         }
 
         private void ImportJsonButton_Click(object sender, RoutedEventArgs e)
 		{
-			UpdateTables(ImportFromJSON("Json/masterJson.json"));
+			OpenFileDialog dialog = new OpenFileDialog();
+			dialog.FileName = "Import JSON file";
+			dialog.DefaultExt = ".json";
+			dialog.Filter = "JSON file (.json)|*.json";
+
+			if (dialog.ShowDialog() == true)
+				UpdateTables(ImportFromJSON(dialog.FileName));
 		}
 
         private void ImportXmlButton_Click(object sender, RoutedEventArgs e)
-        {
-			UpdateTables(ImportFromXML("XML/masterXML.xml"));
+		{
+			OpenFileDialog dialog = new OpenFileDialog();
+			dialog.FileName = "Import XML file";
+			dialog.DefaultExt = ".xml";
+			dialog.Filter = "XML file (.xml)|*.xml";
+
+			if (dialog.ShowDialog() == true)
+				UpdateTables(ImportFromXML(dialog.FileName));
         }
 
         private void ExportCsvButton_Click(object sender, RoutedEventArgs e)
-        {
-            ExportToCSV(LocalizationItems, "CSV", "masterCSV");
+		{
+			SaveFileDialog dialog = new SaveFileDialog();
+			dialog.FileName = "Export your CSV file";
+			dialog.DefaultExt = ".csv";
+			dialog.Filter = "CSV file (.csv)|*.csv";
+
+			if (dialog.ShowDialog() == true)
+			{
+				string fullPath = dialog.FileName;
+
+				string directoryPath = System.IO.Path.GetDirectoryName(fullPath);
+				string fileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(fullPath);
+
+				ExportToCSV(LocalizationItems, directoryPath, fileNameWithoutExtension);
+			}
         }
 
         private void ExportJsonButton_Click(object sender, RoutedEventArgs e)
 		{
-			ExportToJSON(LocalizationItems, "Json", "masterJson");
+			SaveFileDialog dialog = new SaveFileDialog();
+			dialog.FileName = "Export your JSON file";
+			dialog.DefaultExt = ".json";
+			dialog.Filter = "JSON file (.json)|*.json";
+
+			if (dialog.ShowDialog() == true)
+			{
+				string fullPath = dialog.FileName;
+
+				string directoryPath = System.IO.Path.GetDirectoryName(fullPath);
+				string fileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(fullPath);
+
+				ExportToJSON(LocalizationItems, directoryPath, fileNameWithoutExtension);
+			}
 		}
 
         private void ExportXmlButton_Click(object sender, RoutedEventArgs e)
-        {
-            ExportToXML(LocalizationItems, "XML", "masterXML");
+		{
+			SaveFileDialog dialog = new SaveFileDialog();
+			dialog.FileName = "Export your XML file";
+			dialog.DefaultExt = ".xml";
+			dialog.Filter = "XML file (.xml)|*.xml";
+
+			if (dialog.ShowDialog() == true)
+			{
+				string fullPath = dialog.FileName;
+
+				string directoryPath = System.IO.Path.GetDirectoryName(fullPath);
+				string fileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(fullPath);
+
+				ExportToXML(LocalizationItems, directoryPath, fileNameWithoutExtension);
+			}
         }
 
         private void ExportCsButton_Click(object sender, RoutedEventArgs e)
         {
-            GenerateClass(EPlugin.CS, LocalizationItems);
-        }
+			OpenFolderDialog dialog = new OpenFolderDialog();
+
+			dialog.Multiselect = false;
+			dialog.Title = "Select a folder";
+
+			if (dialog.ShowDialog() == true)
+			{
+				GenerateClass(EPlugin.CS, LocalizationItems, dialog.FolderName);
+				ExportToJSON(LocalizationItems, dialog.FolderName, "autoSave");
+			}
+		}
 
         private void ExportHCppButton_Click(object sender, RoutedEventArgs e)
-        {
-            GenerateClass(EPlugin.H_CPP, LocalizationItems);
-        }
+		{
+			OpenFolderDialog dialog = new OpenFolderDialog();
+
+			dialog.Multiselect = false;
+			dialog.Title = "Select a folder";
+
+			if (dialog.ShowDialog() == true)
+			{
+				GenerateClass(EPlugin.H_CPP, LocalizationItems, dialog.FolderName);
+				ExportToJSON(LocalizationItems, dialog.FolderName, "autoSave");
+			}
+		}
 
         private void ExportHInlineButton_Click(object sender, RoutedEventArgs e)
-        {
-            GenerateClass(EPlugin.HInline, LocalizationItems);
-        }
+		{
+			OpenFolderDialog dialog = new OpenFolderDialog();
+
+			dialog.Multiselect = false;
+			dialog.Title = "Select a folder";
+
+			if (dialog.ShowDialog() == true)
+			{
+				GenerateClass(EPlugin.HInline, LocalizationItems, dialog.FolderName);
+				ExportToJSON(LocalizationItems, dialog.FolderName, "autoSave");
+			}
+		}
 
         private void ExportHppCppButton_Click(object sender, RoutedEventArgs e)
-        {
-            GenerateClass(EPlugin.HPP_CPP, LocalizationItems);
-        }
+		{
+			OpenFolderDialog dialog = new OpenFolderDialog();
+
+			dialog.Multiselect = false;
+			dialog.Title = "Select a folder";
+
+			if (dialog.ShowDialog() == true)
+			{
+				GenerateClass(EPlugin.HPP_CPP, LocalizationItems, dialog.FolderName);
+				ExportToJSON(LocalizationItems, dialog.FolderName, "autoSave");
+			}
+		}
 
         private void ExportHppInlineButton_Click(object sender, RoutedEventArgs e)
-        {
-            GenerateClass(EPlugin.HPPInline, LocalizationItems);
+		{
+			OpenFolderDialog dialog = new OpenFolderDialog();
+
+			dialog.Multiselect = false;
+			dialog.Title = "Select a folder";
+
+			if (dialog.ShowDialog() == true)
+			{
+				GenerateClass(EPlugin.HPPInline, LocalizationItems, dialog.FolderName);
+				ExportToJSON(LocalizationItems, dialog.FolderName, "autoSave");
+			}
         }
 
         private void RemoveCurrentLanguage_Click(object sender, RoutedEventArgs e)
